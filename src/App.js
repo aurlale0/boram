e49769c293842c3fb2c5e996a16d4962fb21b98b
aurlale0 <reserved_@hanmail.net>
2019-11-19 17:35:13 +0900

    중간저장

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Customer from './components/Customer'
 import './App.css';
+import { cipher } from 'node-forge';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>또 다시 시작하는 리액트</h2>
-      </header>
-    </div>
-  );
+const customers = [
+  {
+    'idx':1,
+    'image':'https://placeimg.com/64/64/23',
+    'name':'김보람',
+    'birthday':'000506',
+    'gender':'남자',
+    'class':'채널매니저'
+  },
+  {
+    'idx':2,
+    'image':'https://placeimg.com/64/64/24',
+    'name':'이조은',
+    'birthday':'000125',
+    'gender':'여자',
+    'class':'회사원'
+  },
+  {
+    'idx':3,
+    'image':'https://placeimg.com/64/64/22',
+    'name':'임세빈',
+    'birthday':'990501',
+    'gender':'여자',
+    'class':'회사원'
+  }
+]
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        {customers.map(c=>{
+          return <Customer key={c.idx} idx={c.idx} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} class={c.class}/>
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
